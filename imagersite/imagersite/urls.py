beb818687ff308b1bdd630a128bcbf55ded52efc
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2017-11-27 17:31:22 -0800

    Added files for registration.

--- imagersite/imagersite/urls.py
+++ imagersite/imagersite/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from imagersite.views import home_view
@@ -23,4 +23,5 @@ urlpatterns = [
     url(r'^$', home_view, name='home'),
     url(r'^login/', auth_views.login, name='login'),
     url(r'^logout/', auth_views.logout, name='logout'),
+    url(r'^register/', include('registration.backends.simple.urls'), name='register'),
 ]
--- imagersite/imagersite/views.py
+++ imagersite/imagersite/views.py
@@ -6,13 +6,3 @@ from django.shortcuts import render
 def home_view(request):
     """Home page view."""
     return render(request, 'imagersite/home.html')
-
-
-# def login_view(request):
-#     """Login page view."""
-#     return render(request, 'imagersite/login.html')
-
-
-def logout_view(request):
-    """Logout page view."""
-    return render(request, 'imagersite/logout.html')
