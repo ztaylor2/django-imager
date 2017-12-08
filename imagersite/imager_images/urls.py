0f36bf96861917b517a66f87939e8378aa67f319
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2017-12-07 16:01:17 -0800

    Created album and photo gallary views.

--- imagersite/imager_images/urls.py
+++ imagersite/imager_images/urls.py
@@ -1,7 +1,7 @@
 """Imager profile app urls."""
 from django.conf.urls import url
 
-from imager_images.views import LibraryView, PhotoView, AlbumView
+from imager_images.views import LibraryView, PhotoView, AlbumView, AlbumGalleryView, PhotoGalleryView
 
 urlpatterns = [
     url(r'^library/',
@@ -10,7 +10,9 @@ urlpatterns = [
     url(r'^albums/(?P<pk>\d+)/$',
         AlbumView.as_view(template_name='imager_images/album.html'),
         name='album'),
+    url(r'^albums/$', AlbumGalleryView.as_view(), name='albums'),
     url(r'^photos/(?P<pk>\d+)/$',
         PhotoView.as_view(template_name='imager_images/photo.html'),
         name='photo'),
+    url(r'^photos/$', PhotoGalleryView.as_view(), name='albums'),
 ]
--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -40,6 +40,32 @@ class PhotoView(TemplateView):
         return {'photo': photo}
 
 
+class PhotoGalleryView(ListView):
+    """All photos view."""
+
+    model = ImagerProfile
+    template_name = 'imager_images/photo_gallery.html'
+    context_object_name = 'data'
+
+    def get_context_data(self, **kwargs):
+        """."""
+        context = super(PhotoGalleryView, self).get_context_data(**kwargs)
+        photos = context['data'][2].photo.all()
+        albums = context['data'][2].album.all()
+        # import pdb; pdb.set_trace()
+        num_photos = len(photos)
+        num_albums = len(albums)
+
+        context['data'] = {
+            'num_photos': num_photos,
+            'num_albums': num_albums,
+            'photos': photos,
+            'albums': albums,
+        }
+
+        return context
+
+
 class AlbumView(TemplateView):
     """Album view class based view."""
 
@@ -47,3 +73,29 @@ class AlbumView(TemplateView):
         """Get context data for view."""
         album = Album.objects.get(id=pk)
         return {'album': album}
+
+
+class AlbumGalleryView(ListView):
+    """All albums based view."""
+
+    model = ImagerProfile
+    template_name = 'imager_images/album_gallery.html'
+    context_object_name = 'data'
+
+    def get_context_data(self, **kwargs):
+        """."""
+        context = super(AlbumGalleryView, self).get_context_data(**kwargs)
+        photos = context['data'][2].photo.all()
+        albums = context['data'][2].album.all()
+        # import pdb; pdb.set_trace()
+        num_photos = len(photos)
+        num_albums = len(albums)
+
+        context['data'] = {
+            'num_photos': num_photos,
+            'num_albums': num_albums,
+            'photos': photos,
+            'albums': albums,
+        }
+
+        return context
