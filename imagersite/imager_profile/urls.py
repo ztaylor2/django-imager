f98806d7ed73ee5fcc7b099942557c63707b7a71
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2017-12-13 12:10:34 -0800

    Pushing for aws deployment.

--- imagersite/imager_profile/urls.py
+++ imagersite/imager_profile/urls.py
@@ -1,7 +1,7 @@
 """Imager profile app urls."""
 from django.conf.urls import url
 
-from imager_profile.views import GuestView, ProfileView
+from imager_profile.views import GuestView, ProfileView, UpdateProfile
 
 urlpatterns = [
     url(r'^$',
@@ -10,4 +10,6 @@ urlpatterns = [
     url(r'^(?P<username>\w+)/$',
         GuestView.as_view(template_name='imager_profile/guest_profile.html'),
         name='user_profile'),
+    url(r'^edit/(?P<pk>\d+)$', UpdateProfile.as_view(),
+        name='update_profile')
 ]
--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -1,32 +1,27 @@
-from django.shortcuts import render
+"""Views for profile."""
 from django.views import generic
 from imager_profile.models import ImagerProfile
-
 from django.contrib.auth.models import User
 from django.views.generic import TemplateView
-
 from imager_images.models import Photo
-
 from django.shortcuts import get_object_or_404
+from django.core.urlresolvers import reverse_lazy
 
 
 class ProfileView(generic.ListView):
     """Profile page view."""
+
     model = ImagerProfile
     template_name = 'imager_profile/profile.html'
     context_object_name = 'data'
 
     def get_context_data(self, **kwargs):
-        """."""
+        """Get data."""
         context = super(ProfileView, self).get_context_data(**kwargs)
         photos = context['data'][2].photo.all()
         photo_url = photos[0].image.url
         num_photos = len(photos)
         num_albums = context['data'][2].album.all().count()
-        # num_public_images =
-        # num_private_images =
-        # num_public_albums =
-        # num_private_albums =
         context['data'] = {
             'photo_url': photo_url,
             'num_photos': num_photos,
@@ -38,6 +33,7 @@ class ProfileView(generic.ListView):
 
 class GuestView(TemplateView):
     """Guest profile page view."""
+
     model = ImagerProfile
     template_name = 'imager_profile/guest_profile.html'
     context_object_name = 'data'
@@ -50,3 +46,12 @@ class GuestView(TemplateView):
         photo = Photo.objects.order_by('?').first()
         return {'photo': photo,
                 'user': user}
+
+
+class UpdateProfile(generic.UpdateView):
+    """Update profile view."""
+
+    model = ImagerProfile
+    template_name = 'imager_profile/update_profile.html'
+    fields = ['website', 'fee', 'camera', 'services', 'bio', 'phone', 'photo_styles']
+    success_url = reverse_lazy('profile')
