8dea38d97b41f062ba103285836fb36a0e3d00f4
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2017-12-14 18:54:38 -0800

    fixed random photo generator.

--- imagersite/imagersite/views.py
+++ imagersite/imagersite/views.py
@@ -3,10 +3,12 @@ from django.views import generic
 from imager_profile.models import ImagerProfile
 import random
 from django.conf import settings
+from imager_images.models import Photo
 
 
 class HomeView(generic.ListView):
     """Home page view."""
+
     model = ImagerProfile
     template_name = 'imagersite/home.html'
     context_object_name = 'data'
@@ -14,15 +16,9 @@ class HomeView(generic.ListView):
     def get_context_data(self, **kwargs):
         """."""
         context = super(HomeView, self).get_context_data(**kwargs)
-        if context['data']:
-            while True:
-                photos = random.choice(context['data']).photo.all()
-                try:
-                    rand_photo_index = random.randint(0, len(photos) - 1)
-                except ValueError:
-                    continue
-                photo_url = photos[rand_photo_index].image.url
-                break
+        photos = Photo.objects.all()
+        if photos:
+            photo_url = random.choice(photos).image.url
         else:
             photo_url = settings.STATIC_URL + 'imagersite/noimageavailable.png'
         context['data'] = {
